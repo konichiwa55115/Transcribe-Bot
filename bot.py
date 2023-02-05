c7753e15376e9e7112d3be265664bdda400bd479
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-05 23:12:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,9 +13,9 @@ from pyromod import listen
 
 r = sr.Recognizer()
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-API_ID = os.environ.get("API_ID")
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = "6089522865:AAG0vUsYSBg-BaSfiG1T5E1QTrorjoMDA4U"
+API_ID = "17983098"
+API_HASH = "ee28199396e0925f1f44d945ac174f64"
 
 Bot = Client(
     "TranscribeBot",
